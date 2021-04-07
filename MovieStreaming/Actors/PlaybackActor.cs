304f40124cb810153ce009a40a0e9070564f145a
Jorge Moreno <v-jomoreno@youngliving.com>
2021-04-07 17:26:54 -0500

    PlaybackActor and PlayMovieMessage

---
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,24 @@
+﻿
+using Akka.Actor;
+using MovieStreaming.Messages;
+using System;
+
+namespace MovieStreaming.Actors
+{
+    public class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating PlaybackActor");
+
+            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message), message => message.UserId == 123);
+        }
+
+        private void HandlePlayMovieMessage(PlayMovieMessage message) {
+            Console.WriteLine($"Received movie title: {message.MovieTitle}");
+            Console.WriteLine($"Received user id: {message.UserId}");
+        }
+
+        
+    }
+}
--- MovieStreaming/Program.cs
+++ MovieStreaming/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Akka.Actor;
+using MovieStreaming.Actors;
+using MovieStreaming.Messages;
 using System;
 
 namespace MovieStreaming
@@ -10,6 +12,13 @@ namespace MovieStreaming
         static void Main(string[] args)
         {
             MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor system created");
+            Props playbackProps = Props.Create<PlaybackActor>();
+            IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackProps, "PlaybackActor");
+
+            playbackActorRef.Tell(new PlayMovieMessage("Akka.NET The Movie", 123));
+            
+
             Console.ReadLine();
             MovieStreamingActorSystem.Terminate();
         }
