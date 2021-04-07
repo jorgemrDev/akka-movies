7e06981e33faf2380fd8d4645ca311a8a3c71242
Jorge Moreno <v-jomoreno@youngliving.com>
2021-04-07 16:29:08 -0500

    initial commit

---
+++ MovieStreaming/Program.cs
@@ -0,0 +1,17 @@
+﻿using Akka.Actor;
+using System;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+
+        private static ActorSystem MovieStreamingActorSystem;
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.ReadLine();
+            MovieStreamingActorSystem.Terminate();
+        }
+    }
+}
